42e727613648f79a05f4f6b612fab6bd896e6d99
Akash <ag12216@gmail.com>
2019-09-21 12:21:33 -0700

    images added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,16 +4,16 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
- styles:[
-   `
-   a{
-     margin-right: 2em;
-    margin-left: 2em;
-   }
-   `
- ]
+  styles:[
+    `
+    ul li a{
+      color: white;
+    }
+    `
+  ]
+
 })
 export class AppComponent {
   title = 'medicalWeb';
-  menus = ["ABOUT US","PRODUCTS","R&D(DSIR)","CSR","BUSINESS DEVELOPMENT","NEWS AND EVENTS","CAREERS","CONTACT US"];
+  menus = ["ABOUT US","PRODUCTS","R&D(DSIR)","CSR","BUSINESS & DEVELOPMENT","NEWS AND EVENTS","CAREERS","CONTACT US"];
 }
