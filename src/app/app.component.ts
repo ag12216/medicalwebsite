2ed869ad3aa3ccc52c1e8cf582f41b0051d12fc6
Akash <ag12216@gmail.com>
2019-09-22 10:33:37 -0700

    All done hyperlinks to be integrated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,9 +10,7 @@ import { Component } from '@angular/core';
       color: white;
     }
 
-    ul li a:hover{
-     
-    }
+    
     `
   ]
 
@@ -21,4 +19,5 @@ export class AppComponent {
   title = 'medicalWeb';
   menus = ["ABOUT US","PRODUCTS","R&D(DSIR)","CSR","BUSINESS & DEVELOPMENT","NEWS AND EVENTS","CAREERS","CONTACT US"];
   companyName = 'Ashirwad shipping and logistics pvt ltd';
+  arr = [1,2,3,4,5,6,7,8,9,10,11,12,113,14,15,16,17,1,8];
 }
